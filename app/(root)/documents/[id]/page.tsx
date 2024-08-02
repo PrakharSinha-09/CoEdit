05535a726df35a0cd03b9659d86f3bfad20cb8c4
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-08-02 15:36:35 +0530

    improving collaboration functionality via rooms

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,17 +1,30 @@
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import React from 'react'
+import CollaborativeRoom from '@/components/CollaborativeRoom'
+import { getDocument } from '@/lib/actions/room.actions'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
-const Document = () => {
+const Document = async ({params: {id}}:SearchParamProps) => {
+  const clerkUser=await currentUser()
+  if(!clerkUser) redirect('/')
+
+    const room=await getDocument({
+      roomId:id,
+      userId:clerkUser.emailAddresses[0].emailAddress
+    })
+
+    if(!room){
+      redirect('/')
+    }
+
+    //todo: assess the peremision of user to access the doc.
+
   return (
-    <div>
-      <Header >
-        <div className='flex w-fit items-center justify-center gap-2'>
-          <p className='document-title'>document title</p>
-        </div>
-      </Header >
-      <Editor />
-    </div>
+    <main className='flex w-full flex-col items-center'>
+      <CollaborativeRoom 
+       roomId={id}
+       roomMetadata={room.metadata}
+      />
+    </main>
   )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,47 @@
+import AddDocumentBtn from '@/components/AddDocumentBtn';
+import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import Image from 'next/image';
 
-const page = () => {
+const Home =async () => {
+  const clerkUser=await currentUser()
+  if(!clerkUser){
+    redirect('/sign-in')
+  }
+  const documents=[]
   return (
-   <Button>hello </Button>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          {/* <Notifications /> */} Notifications
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {
+        documents.length > 0 ? (
+          <div>
+
+          </div>
+        ):(
+          <div>
+            <Image
+             src="/assets/icons/doc.svg"
+             alt="Document"
+             width={40}
+             height={40}
+             className="mx-auto"
+            />
+            <AddDocumentBtn userId={clerkUser.id} email={clerkUser.emailAddresses[0].emailAddress}/>
+          </div>
+        )
+      }
+   </main>
   )
 }
 
-export default page
+export default Home
