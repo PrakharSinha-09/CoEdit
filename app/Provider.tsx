714ca0648fad428a7a72de62098f17bad5aa9b69
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-08-02 16:19:34 +0530

    now users can mention peoples right in the comments

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,15 +1,30 @@
 'use client'
 
 import {ReactNode} from 'react'
+import { useUser } from '@clerk/nextjs';
 import {LiveblocksProvider, ClientSideSuspense} from '@liveblocks/react/suspense'
 import Loader from '@/components/Loader'
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.actions';
 
 const Provider = ({children}:{children:ReactNode}) => {
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider 
     authEndpoint="/api/liveblocks-auth"
     resolveUsers={async({userIds})=>{
-      // const users=await getClerkUser()
+      const users = await getClerkUsers({ userIds});
+
+      return users;
+    }}
+    resolveMentionSuggestions={async ({ text, roomId }) => {
+      const roomUsers = await getDocumentUsers({
+        roomId,
+        currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+        text,
+      })
+
+      return roomUsers;
     }}
     >
       <ClientSideSuspense fallback={<Loader />}>
